0b8be31b0ba13b4e233822309e1caa9ee76eae85
Beatriz2101 <bpmag21@gmail.com>
2020-06-05 23:54:10 +0200

    Update nav.js

--- assets/nav.js
+++ assets/nav.js
@@ -4,3 +4,44 @@ const nav = document.getElementsByClassName("nav-mobile-final")[0];
 burgerBtn.addEventListener('click', function(){
 	nav.classList.toggle("mobile-toggle");
 });
+
+
+function resizeSubMenu(menuId) {
+    // get the width of the browser window
+    const viewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    
+    //get the subMenu 
+    const subMenu = document.getElementById(menuId);
+
+    //set the submenu width equals to the window width
+    subMenu.style.width = viewportWidth+'px';
+
+    //get the LI element parent of the submenu
+    const parentMenu = subMenu.parentElement;
+
+    //set the submenu width equals to the absolute left position of the parent LI
+    subMenu.style.left = -parentMenu.offsetLeft+'px';
+
+    //get the UL child of the submenu
+    const subMenuList = subMenu.children[0];
+
+    //compute the absolute width of the parent LI width
+    const parentMenuWidth = getComputedStyle(parentMenu).width.replace('px', '');
+
+    //center the UL child related to the parent LI
+    subMenuList.style.marginLeft = ((parentMenu.offsetLeft + (parentMenuWidth/2)) - (450 / 2))+'px'
+}
+
+function resizeSubMenus() {
+    resizeSubMenu('submenu1');
+    resizeSubMenu('submenu2');
+}
+
+if (document.readyState != 'loading'){
+    resizeSubMenus();
+    window.addEventListener('resize', resizeSubMenus);
+
+  } else {
+    document.addEventListener('DOMContentLoaded', resizeSubMenus);
+    window.addEventListener('resize', resizeSubMenus);
+  }
