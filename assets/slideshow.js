6be1ad237e0742d1928ec7ca7e269a5be68e0c07
Miroslav-H <mh.elvete@gmail.com>
2020-06-07 18:41:42 +0200

    stuff

--- assets/slideshow.js
+++ assets/slideshow.js
@@ -18,4 +18,25 @@ function showSlides(n, no) {
   x[slideIndex[no]-1].style.display = "block";  
 }
 
+/*
 
+
+
+function plusSlides(n, no) {
+ 
+	function showSlides(slideIndex[no] += n, no) {
+		var i;
+		var x = document.getElementsByClassName(slideId[no]);
+		
+		if (n > x.length) {slideIndex[no] = 1}    
+		
+		if (n < 1) {slideIndex[no] = x.length}
+		
+		for (i = 0; i < x.length; i++) {
+			x[i].style.display = "none";  
+		}
+		
+		x[slideIndex[no]-1].style.display = "block";  
+	}
+}
+*/
